965b1568f407ecf780a7eaef3293b3bb80d22356
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2023-11-26 18:53:45 +0600

    working on it

---
+++ encapsulation_in_dart/test/encapsulation_in_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:encapsulation_in_dart/encapsulation_in_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
